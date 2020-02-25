90c5cfaa7fcf94b0460b09846441e34078218f4c
prenon9116 <prenon9116@gmail.com>
2020-02-25 09:56:41 -0500

    Before and after anotation added in BaseTest

--- src/main/java/com/bit/test/BaseTest.java
+++ src/main/java/com/bit/test/BaseTest.java
@@ -4,18 +4,24 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BaseTest {
 	
-	@Test
+	WebDriver driver;
+	
+	@BeforeMethod 
 	public void openBrowser(String url) {
 		System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+	
+	public void tearDown() {
+		driver.quit();
 	}
 
 }
