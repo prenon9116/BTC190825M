2e03e180811a56db51a3aa7ccb37c90062849c4e
prenon9116 <prenon9116@gmail.com>
2020-02-24 22:28:02 -0500

    Commit from master branch 2nd time

--- src/main/java/com/bit/test/BaseTest.java
+++ src/main/java/com/bit/test/BaseTest.java
@@ -1,9 +1,19 @@
 package com.bit.test;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
 	
-	public void openBrowser() {
-		
+	public void openBrowser(String url) {
+		System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 
 }
